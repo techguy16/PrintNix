fa317af5d5282431b923019b8745f286907e72df
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-10-15 17:57:22 +1300

    Add files via upload

---
+++ epson/stylus-tx420/epson-nx420-install.sh
@@ -0,0 +1,4 @@
+sudo dpkg -i iscan-data_1.39.2-1_all.deb
+sudo dpkg -i iscan_2.30.4-2_amd64.deb
+sudo dpkg -i iscan-network-nt_1.1.2-1_amd64.deb
+sudo dpkg -i epson-inkjet-printer-nx420_1.0.0-1lsb3.2_amd64.deb
